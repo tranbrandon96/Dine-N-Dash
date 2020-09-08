c5dcf03166c23074920ef88d4ccf645ec84efcac
manuelb0603 <50155726+manuelb0603@users.noreply.github.com>
2020-09-08 14:34:53 -0700

    Added Custom Icon and Name, Added New Screens, Reordered app to open in order

--- lib/LoginActivity.dart
+++ lib/LoginActivity.dart
@@ -1,157 +1,201 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/SignUpSelectActivity.dart';
+import 'CustomerIDActivity.dart';
+import 'AccountCreationActivity.dart';
 
 
-class LoginActivity extends MaterialPageRoute<Null> {
-  LoginActivity() : super(builder: (BuildContext context){
-  return Scaffold(
+class LoginActivity extends StatefulWidget {
+  LoginActivity({Key key, this.title}) : super(key: key);
 
-    body: Container(
-    decoration: BoxDecoration(
-    gradient: LinearGradient(
-    begin: Alignment.topRight,
-    end: Alignment.bottomLeft,
-    colors: [Colors.pink, Colors.deepOrangeAccent]),
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
 
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
-    ),
+  final String title;
 
+  @override
+  _LoginActivity createState() => _LoginActivity();
+}
 
-    child: Center(
+class _LoginActivity extends State<LoginActivity> {
+  int _counter = 0;
 
-    // Center is a layout widget. It takes a single child and positions it
-    // in the middle of the parent.
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
 
-
-    child: Column(
-    // Column is also a layout widget. It takes a list of children and
-    // arranges them vertically. By default, it sizes itself to fit its
-    // children horizontally, and tries to be as tall as its parent.
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
     //
-    // Invoke "debug painting" (press "p" in the console, choose the
-    // "Toggle Debug Paint" action from the Flutter Inspector in Android
-    // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-    // to see the wireframe for each widget.
-    //
-    // Column has various properties to control how it sizes itself and
-    // how it positions its children. Here we use mainAxisAlignment to
-    // center the children vertically; the main axis here is the vertical
-    // axis because Columns are vertical (the cross axis would be
-    // horizontal).
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
 
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-    children: <Widget>[
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.pink, Colors.deepOrangeAccent]),
 
 
-      Image(
-
-        image: AssetImage('assets/images/DnD Logo.png'),
-        width: 250,
-        height: 250,
-      ),
-
-      SizedBox(height: 20 ),
-      Text(
-        'USERNAME',
-        style: TextStyle(fontSize: 18, color: Colors.white),
-
-      ),
+        ),
 
 
-      Container(
-        width: 300,
-        child: TextField(
-          obscureText: false,
+        child: Center(
 
-          style: TextStyle(
-              fontSize: 20,
-              color: Colors.white
-          ),
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
 
-          decoration: InputDecoration(
-            enabledBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.white, width:1.0),
-            ),
-            focusedBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.white, width:1.0),
-            ),
-            border: UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.white, width:1.0),
 
-            ),
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+
+            children: <Widget>[
+
+
+              Image(
+
+                image: AssetImage("assets/images/DnD Logo.PNG"),
+                width: 250,
+                height: 250,
+              ),
+
+              SizedBox(height: 20),
+              Text(
+                'USERNAME',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+
+              ),
+
+
+              Container(
+                width: 300,
+                child: TextField(
+                  obscureText: false,
+
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white
+                  ),
+
+                  decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1.0),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1.0),
+                    ),
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1.0),
+
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 20),
+              Text(
+                'PASSWORD',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+
+              ),
+
+              Container(
+                width: 300,
+                child: TextField(
+                  obscureText: true,
+
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white
+                  ),
+
+                  decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1.0),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1.0),
+                    ),
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1.0),
+
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+
+
+              RaisedButton(onPressed: () {
+                Navigator.push(context, CustomerIDActivity());
+              },
+                child: Text(
+                    'LOGIN',
+                    style: TextStyle(color: Colors.deepOrange)),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
+
+              ),
+
+
+              FlatButton(onPressed: () {},
+                child: Text(
+                    'Forgot Username or Password?',
+                    style: TextStyle(color: Colors.white)),
+                highlightColor: Colors.deepOrangeAccent,
+              ),
+
+
+              FlatButton(onPressed: () {
+                Navigator.push(context, SignUpSelectActivity());
+              },
+                child: Text(
+                    'No Account? SIGN UP',
+                    style: TextStyle(color: Colors.white)),
+                highlightColor: Colors.deepOrangeAccent,
+              ),
+              SizedBox(height: 40),
+
+
+            ],
+
           ),
         ),
       ),
-
-      SizedBox(height: 20 ),
-      Text(
-        'PASSWORD',
-        style: TextStyle(fontSize: 18, color: Colors.white),
-
-      ),
-
-      Container(
-        width: 300,
-        child: TextField(
-          obscureText: true,
-
-          style: TextStyle(
-              fontSize: 20,
-              color: Colors.white
-          ),
-
-          decoration: InputDecoration(
-            enabledBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.white, width:1.0),
-            ),
-            focusedBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.white, width:1.0),
-            ),
-            border: UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.white, width:1.0),
-
-            ),
-          ),
-        ),
-      ),
-      SizedBox(height: 20 ),
-
-
-      RaisedButton(onPressed: () {},
-        child: Text(
-            'LOGIN',
-            style: TextStyle(color: Colors.deepOrange)),
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(18.0),
-        ),
-
-      ),
-
-
-      FlatButton(onPressed: () {},
-        child: Text(
-            'Forgot Username or Password?',
-            style: TextStyle(color: Colors.white)),
-        highlightColor: Colors.deepOrangeAccent,
-      ),
-
-
-      FlatButton(onPressed: () {},
-        child: Text(
-            'No Account? SIGN UP',
-            style: TextStyle(color: Colors.white)),
-        highlightColor: Colors.deepOrangeAccent,
-      ),
-      SizedBox(height: 40 ),
-
-
-
-
-    ],
-
-    ),
-    ),
-    ),
     );
-  });
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/LoginActivity.dart';
 import 'LoginActivity.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(DineNDashApp());
 }
 
-class MyApp extends StatelessWidget {
+class DineNDashApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -28,161 +27,8 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginActivity(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-
-      body:  Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                colors: [Colors.pink, Colors.deepOrangeAccent]),
-
-
-
-
-    ),
-
-
-        child: Center(
-
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-
-
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-
-          mainAxisAlignment: MainAxisAlignment.center,
-
-          children: <Widget>[
-             Align(alignment: Alignment.topRight,
-                child: IconButton(onPressed: () {
-                  Navigator.push(context,LoginActivity());
-                },
-                  icon: Icon(Icons.cancel,size:35, color:Colors.white),)),
-
-            Text(
-              'CUSTOMER CHECK IN',
-              style: TextStyle(fontSize: 27, color: Colors.white),
-
-            ),
-            SizedBox(height: 20 ),
-
-            Container(
-              height: 400,
-              width: 300,
-
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.black,
-                  ),
-                  borderRadius: BorderRadius.circular(20)),
-
-              child: Container(
-
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-
-                      Text(
-                        'Member Card',
-
-                        style: TextStyle(fontSize: 23, color: Colors.deepOrange, fontWeight: FontWeight.bold),
-                      ),
-                      Image(
-
-                        image: AssetImage('assets/images/QR_Code.png'),
-                      ),
-
-                      Text(
-                        'John Doe',
-                        style: TextStyle(fontSize: 23, color: Colors.deepOrange, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 5 ),
-                      Text(
-                        '7HSGD930GJAJADMG39J',
-                        style: TextStyle(fontSize: 15, color: Colors.deepOrange),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-
-            SizedBox(height: 20 ),
-          Align(
-            alignment: Alignment.center,
-            child: Text(
-              "Scan when arriving to restaurant to get the Dine n' Dash experience!",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 14, color: Colors.white),
-            ),
-          ),
-          ],
-
-        ),
-      ),
-      ),
-    );
-  }
-}
