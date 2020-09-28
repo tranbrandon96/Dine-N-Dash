4f99647f7d8b1132a68577854330653cc6702043
Brandon Tran <brandon.tran@student.csulb.edu>
2020-09-28 13:59:21 -0700

    Revert "Default Futura Font is implemeted in Application"
    This reverts commit 16e5639c7619a002c6b805505ceee78aa06d4fde.

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,8 @@ class DineNDashApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        fontFamily: "Futura",
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
