a5211c8dd51ab7777e1cd05eaa4faf226b9fe010
manuelb0603 <50155726+manuelb0603@users.noreply.github.com>
2020-10-22 10:30:11 -0700

    Fixed typo

--- lib/screens/employee_screens/view_table_screen.dart
+++ lib/screens/employee_screens/view_table_screen.dart
@@ -1,4 +1,4 @@
-cimport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_app/screens/employee_screens/checkout_screen.dart';
